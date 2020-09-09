b3f2741a9fc519cac47b50062ea596cb0675fb72
anveloper <anveloper@gmail.com>
2020-09-09 18:22:33 +0200

    deadonly waves puzzle mechanism

--- src/app/deadly-waves/deadly-waves.component.ts
+++ src/app/deadly-waves/deadly-waves.component.ts
@@ -1,21 +1,32 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {PuzzleSchedulerService} from '../puzzle-scheduler.service';
+import {fromEvent, merge, Observable, of} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'um-deadly-waves',
   template: `
-    <p>
-      deadly-waves works!
-    </p>
+    <ng-container *ngIf="(offline$ | async); else online">
+      Udało się!
+      <button (click)="scheduler.onPuzzleSolved()">Solve!</button>
+    </ng-container>
+
+    <ng-template #online>
+      Maszty są coraz silniejsze! Czuję, że czapka przestaje wystarczać... Muszę jak najszybciej zniszczyć źródło fal.
+    </ng-template>
   `,
   styleUrls: ['./deadly-waves.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DeadlyWavesComponent implements OnInit {
+export class DeadlyWavesComponent {
+  readonly offline$: Observable<boolean>;
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
+  constructor(public scheduler: PuzzleSchedulerService) {
+    this.offline$ = merge(
+      fromEvent(window, 'offline').pipe(map(() => true)),
+      fromEvent(window, 'online').pipe(map(() => false)),
+      of(!navigator.onLine),
+    );
   }
 
 }
