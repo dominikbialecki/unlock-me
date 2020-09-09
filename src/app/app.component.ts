cbe6998dc481a3072ee4fe139941f764a63781d2
anveloper <anveloper@gmail.com>
2020-09-09 17:28:30 +0200

    initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'um-root',
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div>app component works!</div>`,
 })
 export class AppComponent {
-  title = 'unlock-me';
 }
